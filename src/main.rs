a2f0477aa74704328193b0ce9e6ed089f9bd9f85
Maciej Modzelewski <maciej@modzelewski.dev>
2023-03-12 18:25:38 +0100

    Implement getting recent time entries

--- src/main.rs
+++ src/main.rs
@@ -1,42 +1,50 @@
-use std::{collections::HashMap, fs};
+use std::{collections::HashMap, fmt::Display, fs};
 
-use anyhow::{Result, Context};
+use anyhow::{Context, Result};
 use clap::{Parser, Subcommand};
-use reqwest::{
-    blocking::{Client, Response},
-    header::CONTENT_TYPE,
-    Method,
-};
+use reqwest::{blocking::Client, header::CONTENT_TYPE, Method};
+use serde::Deserialize;
 
 #[derive(Parser, Debug)]
 #[command(author, version, about, long_about = None)]
 struct Args {
     #[command(subcommand)]
-    command: Command,
+    command: Option<Command>,
 }
 
 fn main() -> Result<()> {
     let args = Args::parse();
-    println!("{:?}", args.command);
 
     let auth = read_env_variables()?;
 
-    let client = Client::new();
-    let json: Response = client
-        .request(
-            Method::GET,
-            "https://api.track.toggl.com/api/v9/me".to_string(),
-        )
-        .basic_auth(auth.username, Some(auth.password))
-        .header(CONTENT_TYPE, "application/json")
-        .send()?;
-
-    println!("{:?}", json);
+    match args.command {
+        Some(_) => todo!(),
+        None => print_latest_entries(&auth)?,
+    }
 
     return Ok(());
 }
 
-fn read_env_variables<'a>() -> Result<Auth> {
+fn print_latest_entries(auth: &Auth) -> Result<()> {
+    let client = Client::new();
+    let json: Vec<TimeEntry> = client
+        .request(
+            Method::GET,
+            "https://api.track.toggl.com/api/v9/me/time_entries".to_string(),
+        )
+        .basic_auth(&auth.username, Some(&auth.password))
+        .header(CONTENT_TYPE, "application/json")
+        .send()?
+        .json()?;
+
+    for time_entry in json {
+        println!("{}", time_entry);
+    }
+
+    return Ok(());
+}
+
+fn read_env_variables() -> Result<Auth> {
     let env_file = fs::read_to_string(".env").context(".env file should exist")?;
     let variables: HashMap<&str, &str> = env_file
         .lines()
@@ -44,8 +52,14 @@ fn read_env_variables<'a>() -> Result<Auth> {
         .collect();
 
     return Ok(Auth {
-        username: variables.get("USERNAME").context("Username must be provided")?.to_string(),
-        password: variables.get("PASSWORD").context("Password must be provided")?.to_string(),
+        username: variables
+            .get("USERNAME")
+            .context("Username must be provided")?
+            .to_string(),
+        password: variables
+            .get("PASSWORD")
+            .context("Password must be provided")?
+            .to_string(),
     });
 }
 
@@ -58,4 +72,29 @@ struct Auth {
 enum Command {
     Start,
     Stop,
+}
+
+#[derive(Debug, Deserialize)]
+struct TimeEntry {
+    description: Option<String>,
+    start: String,
+    stop: Option<String>,
+}
+
+impl Display for TimeEntry {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        let empty_description = "no description".to_string();
+        let description = match &self.description {
+            Some(desc) => desc,
+            None => &empty_description,
+        };
+
+        let still_running = "in progress".to_string();
+        let stop = match &self.stop {
+            Some(time) => time,
+            None => &still_running,
+        };
+
+        return write!(f, "{}: {} - {}", description, &self.start, stop);
+    }
 }
