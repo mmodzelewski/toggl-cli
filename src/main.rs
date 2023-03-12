9935fdb804f8581a86e60bebb14e67e188dd6709
Maciej Modzelewski <maciej@modzelewski.dev>
2023-03-12 13:37:14 +0100

    Extract reading auth variables to a method. Add anyhow

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,12 @@
 use std::{collections::HashMap, fs};
 
+use anyhow::{Result, Context};
 use clap::{Parser, Subcommand};
-use reqwest::{blocking::Client, header::CONTENT_TYPE, Method};
-use serde_json::Value;
+use reqwest::{
+    blocking::{Client, Response},
+    header::CONTENT_TYPE,
+    Method,
+};
 
 #[derive(Parser, Debug)]
 #[command(author, version, about, long_about = None)]
@@ -11,33 +15,43 @@ struct Args {
     command: Command,
 }
 
-fn main() -> Result<(), reqwest::Error> {
+fn main() -> Result<()> {
     let args = Args::parse();
     println!("{:?}", args.command);
 
-    let env_file = fs::read_to_string(".env").expect(".env file should exist");
+    let auth = read_env_variables()?;
+
+    let client = Client::new();
+    let json: Response = client
+        .request(
+            Method::GET,
+            "https://api.track.toggl.com/api/v9/me".to_string(),
+        )
+        .basic_auth(auth.username, Some(auth.password))
+        .header(CONTENT_TYPE, "application/json")
+        .send()?;
+
+    println!("{:?}", json);
+
+    return Ok(());
+}
+
+fn read_env_variables<'a>() -> Result<Auth> {
+    let env_file = fs::read_to_string(".env").context(".env file should exist")?;
     let variables: HashMap<&str, &str> = env_file
         .lines()
         .filter_map(|line| line.split_once("="))
         .collect();
 
-    let client = Client::new();
-    let json: Value = client
-        .request(
-            Method::GET,
-            "https://api.track.toggl.com/api/v9/me".to_string(),
-        )
-        .basic_auth(
-            variables["USERNAME"],
-            variables.get("PASSWORD")
-        )
-        .header(CONTENT_TYPE, "application/json")
-        .send()?
-        .json()?;
+    return Ok(Auth {
+        username: variables.get("USERNAME").context("Username must be provided")?.to_string(),
+        password: variables.get("PASSWORD").context("Password must be provided")?.to_string(),
+    });
+}
 
-    println!("{:?}", json);
-
-    return Ok(());
+struct Auth {
+    username: String,
+    password: String,
 }
 
 #[derive(Subcommand, Debug)]
