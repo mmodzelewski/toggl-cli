a49e1d926d1873bda44eb85f71cea6b0771088c5
Maciej Modzelewski <maciej@modzelewski.dev>
2023-03-12 11:07:18 +0100

    Add simple command parsing

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
+use clap::{Parser, Subcommand};
+
+#[derive(Parser, Debug)]
+#[command(author, version, about, long_about = None)]
+struct Args {
+    #[command(subcommand)]
+    command: Command,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args = Args::parse();
+    println!("{:?}", args.command);
+}
+
+#[derive(Subcommand, Debug)]
+enum Command {
+    Start,
+    Stop,
 }
