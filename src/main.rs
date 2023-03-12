35137f36948e167e56813f7c3a272616223dd12f
Maciej Modzelewski <maciej@modzelewski.dev>
2023-03-12 21:43:56 +0100

    Implement an option to restart last time entry

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 use std::{collections::HashMap, fmt::Display, fs};
 
 use anyhow::{Context, Ok, Result};
+use chrono::Utc;
 use clap::{Parser, Subcommand};
 use directories::ProjectDirs;
 use reqwest::{
@@ -8,7 +9,7 @@ use reqwest::{
     header::CONTENT_TYPE,
     Method,
 };
-use serde::Deserialize;
+use serde::{Deserialize, Serialize};
 
 #[derive(Parser, Debug)]
 #[command(author, version, about, long_about = None)]
@@ -29,6 +30,7 @@ fn main() -> Result<()> {
             Command::Stop => client.stop_current_entry()?,
             Command::Status => client.print_current_entry()?,
             Command::Recent => client.print_recent_entries()?,
+            Command::Restart => client.restart()?,
         },
         None => client.print_recent_entries()?,
     }
@@ -59,16 +61,20 @@ impl TogglClient {
     }
 
     fn print_recent_entries(&self) -> Result<()> {
-        let time_entries: Vec<TimeEntry> = self
-            .request(Method::GET, "me/time_entries".to_string())
-            .send()?
-            .json()?;
-
+        let time_entries = self.get_recent_entries()?;
         for time_entry in time_entries {
             println!("{}", time_entry);
         }
 
         return Ok(());
+    }
+
+    fn get_recent_entries(&self) -> Result<Vec<TimeEntry>> {
+        return self
+            .request(Method::GET, "me/time_entries".to_string())
+            .send()?
+            .json()
+            .context("Could not get time entries");
     }
 
     fn print_current_entry(&self) -> Result<()> {
@@ -93,7 +99,7 @@ impl TogglClient {
     fn stop_current_entry(&self) -> Result<()> {
         let maybe_time_entry = self.get_current_entry()?;
         if let Some(time_entry) = maybe_time_entry {
-             let time_entry: TimeEntry = self
+            let time_entry: TimeEntry = self
                 .request(
                     Method::PATCH,
                     format!(
@@ -104,7 +110,28 @@ impl TogglClient {
                 .send()?
                 .json()
                 .context("Could not stop the current time entry")?;
-             println!("Stopped time entry: {}", time_entry)
+            println!("Stopped time entry: {}", time_entry)
+        }
+        return Ok(());
+    }
+
+    fn restart(&self) -> Result<()> {
+        let recent_entries = self.get_recent_entries()?;
+        let maybe_last_one = recent_entries.first();
+        if let Some(last_one) = maybe_last_one {
+            let new_time_entry = NewTimeEntry::from_time_entry(last_one)?;
+            let stared_entry: TimeEntry = self
+                .request(
+                    Method::POST,
+                    format!("workspaces/{}/time_entries", last_one.workspace_id),
+                )
+                .json(&new_time_entry)
+                .send()?
+                .json()
+                .context("Could not start a time entry")?;
+            println!("Time entry started: {}", stared_entry);
+        } else {
+            println!("No recent time entry to restart");
         }
         return Ok(());
     }
@@ -143,6 +170,7 @@ enum Command {
     Stop,
     Status,
     Recent,
+    Restart,
 }
 
 #[derive(Debug, Deserialize)]
@@ -150,8 +178,34 @@ struct TimeEntry {
     id: u64,
     workspace_id: u64,
     description: Option<String>,
+    project_id: Option<u64>,
     start: String,
     stop: Option<String>,
+    duration: i64,
+}
+
+#[derive(Serialize)]
+struct NewTimeEntry {
+    workspace_id: u64,
+    created_with: String,
+    description: Option<String>,
+    project_id: Option<u64>,
+    start: String,
+    duration: i64,
+}
+
+impl NewTimeEntry {
+    fn from_time_entry(time_entry: &TimeEntry) -> Result<NewTimeEntry> {
+        let now = Utc::now();
+        return Ok(NewTimeEntry {
+            workspace_id: time_entry.workspace_id,
+            created_with: "toggl-cli".to_string(),
+            description: time_entry.description.to_owned(),
+            project_id: time_entry.project_id.to_owned(),
+            start: format!("{:?}", now),
+            duration: -1 * now.timestamp(),
+        });
+    }
 }
 
 impl Display for TimeEntry {
@@ -167,8 +221,13 @@ impl Display for TimeEntry {
             Some(time) => time,
             None => &still_running,
         };
+        write!(f, "{}: {} - {}", description, &self.start, stop)?;
 
-        return write!(f, "{}: {} - {}", description, &self.start, stop);
+        let duration = &self.duration;
+        if let Some(_) = &self.stop {
+            write!(f, " - {} seconds", duration)?;
+        }
+        return write!(f, "");
     }
 }
 
